48fc86fb7eb7788d2f913aca3c521a4fa844059e
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-10-23 23:15:30 +0200

    Redone migrations and add settings files

---
+++ drife/development.py
@@ -0,0 +1,13 @@
+from drife.common import *
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+WSGI_APPLICATION = 'drife.wsgi.application'
---
+++ drife/production.py
@@ -0,0 +1,14 @@
+from drife.common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'drife',
+        'USER': 'drife',
+        'PASSWORD': 'drife',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+WSGI_APPLICATION = 'drife.wsgi_production.application'
