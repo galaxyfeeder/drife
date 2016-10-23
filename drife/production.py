5f56b4c23d6389c6b0043c99ac83691abe55e397
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-10-23 23:47:00 +0200

    Changed settings

--- drife/production.py
+++ drife/production.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'drife',
         'USER': 'drife',
-        'PASSWORD': 'drife',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
