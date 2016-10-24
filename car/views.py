ad810112aa5971f6bc7370b4ca0e107658584003
Gabriel Esteban Gullon <gabriel.esteban.gullon@gmail.com>
2016-10-24 10:17:51 +0200

    Style on table

--- car/views.py
+++ car/views.py
@@ -13,7 +13,7 @@ class CarDetail(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(CarDetail, self).get_context_data(**kwargs)
-
+        context['last_usages'] = CarUsage.objects.filter(car=self.object)[:10]
         return context
 
 
